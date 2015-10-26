845eb37558bf90accb100f9e71e360799068d2b2
Brett Zamir <brettz9@yahoo.com>
2015-10-25 17:02:06 -0700

    display default in user.js

--- resources/user.js
+++ resources/user.js
@@ -3,6 +3,7 @@ window.addEventListener('DOMContentLoaded', function () {'use strict';
 
     var tb = new TextBrowser({
         // languages: 'bower_components/bahaiwritings/appdata/languages.json', // Default
+        // site: 'site.json' // Default
         files: 'files.json'
     });
     tb.init();
