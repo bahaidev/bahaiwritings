a7a106ddd39afa04189e6a905b5340fcea6845b8
Brett Zamir <brettz9@yahoo.com>
2015-11-09 19:41:28 -0700

    document.domain only works from subdomains

--- resources/user.js
+++ resources/user.js
@@ -1,8 +1,11 @@
 /*globals TextBrowser, URLSearchParams*/
 window.addEventListener('DOMContentLoaded', function () {'use strict';
 
+    /*
+    // This will only work from subdomains; would need postMessage() routine to allow introspection
     var params = new URLSearchParams(location.hash.slice(1));
-    document.domain = params.get('domain') || document.domain; // Let this be iframe-embeddable from any domain
+    document.domain = params.get('domain') || document.domain; // Let this be iframe-embeddable
+    */
     var tb = new TextBrowser({
         // languages: 'bower_components/bahaiwritings/appdata/languages.json', // Default
         // site: 'site.json' // Default
