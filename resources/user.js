d448f1aebc0c43e43ddbfbb560fcd9ba46db0e31
Brett Zamir <brettz9@yahoo.com>
2015-11-09 19:21:48 -0700

    Allow cross-origin iframe embedding

--- resources/user.js
+++ resources/user.js
@@ -1,6 +1,8 @@
-/*globals TextBrowser*/
+/*globals TextBrowser, URLSearchParams*/
 window.addEventListener('DOMContentLoaded', function () {'use strict';
 
+    var params = new URLSearchParams(location.hash.slice(1));
+    document.domain = params.get('domain') || document.domain; // Let this be iframe-embeddable from any domain
     var tb = new TextBrowser({
         // languages: 'bower_components/bahaiwritings/appdata/languages.json', // Default
         // site: 'site.json' // Default
