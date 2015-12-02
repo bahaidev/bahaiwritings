53d77579319413944c24c2b7f5f7f8f2a302d496
Brett Zamir <brettz9@yahoo.com>
2015-12-02 14:20:58 -0700

    Add namespace to avoid storage clashes

--- resources/user.js
+++ resources/user.js
@@ -1,4 +1,4 @@
-/*globals TextBrowser, URLSearchParams*/
+/*globals TextBrowser */
 window.addEventListener('DOMContentLoaded', function () {'use strict';
 
     // This will only work from subdomains; would need postMessage() routine to allow introspection
@@ -9,7 +9,8 @@ window.addEventListener('DOMContentLoaded', function () {'use strict';
     var tb = new TextBrowser({
         // languages: 'bower_components/bahaiwritings/appdata/languages.json', // Default
         // site: 'site.json' // Default
-        files: 'files.json'
+        files: 'files.json',
+        namespace: 'bahaiwritings'
     });
     tb.init();
 
