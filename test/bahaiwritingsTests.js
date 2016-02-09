965a8705891fdfdb48e91b9ed725813faedf5ffe
Brett Zamir <brettz9@yahoo.com>
2016-02-08 18:58:51 -0700

    Add locales and languages tests (languages not working solely due to json-refs path issue)

--- test/bahaiwritingsTests.js
+++ test/bahaiwritingsTests.js
@@ -1,6 +1,9 @@
 
 var appBase = '/bahaiwritings/';
-var schemaBase = appBase + 'bower_components/textbrowser/general-schemas/';
+var textbrowserBase = appBase + 'bower_components/textbrowser/';
+var schemaBase = textbrowserBase + 'general-schemas/';
+var localesBase = textbrowserBase + 'locales/';
+var appdataBase = textbrowserBase + 'appdata/';
 
 function validate (schema, data) {
     var ajv = Ajv();
@@ -33,6 +36,32 @@ var bahaiwritingsTests = {
         Promise.all([
             JsonRefs.resolveRefsAt('site.jsonschema', {relativeBase: schemaBase}),
             JsonRefs.resolveRefsAt('site.json', {relativeBase: appBase})
+        ]).then(function ([{resolved: schema}, {resolved: data}]) {
+            valid = validate(schema, data);
+            test.strictEqual(valid, true);
+            test.done();
+        });
+    },
+    'locales tests': function (test) {
+        test.expect(4);
+        Promise.all([
+            JsonRefs.resolveRefsAt('locale.jsonschema', {relativeBase: schemaBase}),
+            JsonRefs.resolveRefsAt('en-US.json', {relativeBase: localesBase}),
+            JsonRefs.resolveRefsAt('ar.json', {relativeBase: localesBase}),
+            JsonRefs.resolveRefsAt('fa.json', {relativeBase: localesBase}),
+            JsonRefs.resolveRefsAt('ru.json', {relativeBase: localesBase})
+        ]).then(function ([{resolved: schema}, {resolved: enUS}, {resolved: ar}, {resolved: fa}, {resolved: ru}]) {
+            [enUS, ar, fa, ru].forEach(function (locale) {
+                valid = validate(schema, locale);
+                test.strictEqual(valid, true);
+            });
+            test.done();
+        });
+    },
+    'languages.json test': function (test) { // See TextBrowser to-dos on what must be fixed for this to work
+        Promise.all([
+            JsonRefs.resolveRefsAt('languages.jsonschema', {relativeBase: schemaBase}),
+            JsonRefs.resolveRefsAt('languages.json', {relativeBase: localesBase})
         ]).then(function ([{resolved: schema}, {resolved: data}]) {
             valid = validate(schema, data);
             test.strictEqual(valid, true);
