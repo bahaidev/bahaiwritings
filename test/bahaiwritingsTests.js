143afc52d8d31bc2af0379b36e6d96a8733cd5fb
Brett Zamir <brettz9@yahoo.com>
2016-02-13 13:56:44 -0700

    Moved languages/locales tests to textbrowser repo

--- test/bahaiwritingsTests.js
+++ test/bahaiwritingsTests.js
@@ -41,32 +41,6 @@ var bahaiwritingsTests = {
             test.strictEqual(valid, true);
             test.done();
         });
-    },
-    'locales tests': function (test) {
-        test.expect(4);
-        Promise.all([
-            JsonRefs.resolveRefsAt('locale.jsonschema', {relativeBase: schemaBase}),
-            JsonRefs.resolveRefsAt('en-US.json', {relativeBase: localesBase}),
-            JsonRefs.resolveRefsAt('ar.json', {relativeBase: localesBase}),
-            JsonRefs.resolveRefsAt('fa.json', {relativeBase: localesBase}),
-            JsonRefs.resolveRefsAt('ru.json', {relativeBase: localesBase})
-        ]).then(function ([{resolved: schema}, {resolved: enUS}, {resolved: ar}, {resolved: fa}, {resolved: ru}]) {
-            [enUS, ar, fa, ru].forEach(function (locale) {
-                valid = validate(schema, locale);
-                test.strictEqual(valid, true);
-            });
-            test.done();
-        });
-    },
-    'languages.json test': function (test) { // See TextBrowser to-dos on what must be fixed for this to work
-        Promise.all([
-            JsonRefs.resolveRefsAt('languages.jsonschema', {relativeBase: schemaBase}),
-            JsonRefs.resolveRefsAt('languages.json', {relativeBase: localesBase})
-        ]).then(function ([{resolved: schema}, {resolved: data}]) {
-            valid = validate(schema, data);
-            test.strictEqual(valid, true);
-            test.done();
-        });
     }
 };
 
