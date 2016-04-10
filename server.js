d2e67f8f278c278063ed21c19a3644d4b3999f94
Brett Zamir <brettz9@yahoo.com>
2016-04-10 13:17:34 -0700

    Put testing in own port (until we can get the app to work with a port)

--- server.js
+++ server.js
@@ -2,11 +2,12 @@
 'use strict';
 const ns = require('node-static');
 const opn = require('opn');
+const url = require('url');
 
 // const port = 8082; // Todo: Fix so can use this
 const portIdx = 3;
 const defaultPort = 80;
-const port = process.argv[portIdx] || defaultPort;
+const port = parseInt(process.argv[portIdx] || defaultPort, 10);
 
 const file = new ns.Server();
 
@@ -17,10 +18,17 @@ require('http').createServer(function (req, res) {
 }).listen(port);
 
 const siteIdx = 2;
-const site = process.argv[siteIdx] || 'http://127.0.0.1/';
+const defaultURL = 'http://127.0.0.1/';
+let site = process.argv[siteIdx] || defaultURL;
+
+if (port !== defaultPort) {
+    const portAdded = url.parse(site);
+    portAdded.host = portAdded.host.replace(/:.*$/, '') + ':' + port;
+    site = url.format(portAdded);
+}
 
 if (site === '0') {
-    console.log('Started server; open a file within http://127.0.0.1 in the browser. Tests may take a while to load!'); // eslint-disable-line no-console
+    console.log('Started server; open a file within ' + defaultURL + ' in the browser. Tests may take a while to load!'); // eslint-disable-line no-console
 } else {
     console.log('Started server; opening ' + site + ' in the browser.' + (site.match(/test/) ? ' May take a while to load!' : '')); // eslint-disable-line no-console
     opn(site);
