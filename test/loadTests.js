85eaf7a4e8f53ce1be6cdf2d8493c23073bf00f4
Brett Zamir <brettz9@yahoo.com>
2016-10-28 13:18:33 -0700

    Fix for latest nodeunit; shorten description to 140 chars as per Bower limit; remove unneeded resolutions

--- test/loadTests.js
+++ test/loadTests.js
@@ -1,4 +1,4 @@
 /*global nodeunit, bahaiwritingsTests */
 nodeunit.run({
     'Suite One': bahaiwritingsTests
-});
+}, {}); // To-do: Remove last arg after https://github.com/caolan/nodeunit/pull/329
