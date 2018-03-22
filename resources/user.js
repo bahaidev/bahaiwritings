290be02c5419d1092414f78f7500a543e5fe6090
Brett Zamir <brettz9@yahoo.com>
2018-03-22 22:36:57 +0800

    - Rearrange code comment order

--- resources/user.js
+++ resources/user.js
@@ -1,18 +1,18 @@
 import TextBrowser from '../node_modules/textbrowser/dist/index-es.js';
 
+// Todo: Add a favicon file (and remove empty one in HTML?)
+// import loadStylesheets from '../node_modules/load-stylesheets/dist/index-es.js';
+// loadStylesheets('favicon.ico'); // No need for `await`
+
+// Todo: Reenable the following in `files.json`?
+// {"name": "collins", "file": {"$ref": "data/other-works/Collins.json"}, "schemaFile": "Collins.jsonschema", "metadataFile": "Collins.metadata"},
+
 /*
 // This will only work from subdomains; would need postMessage()
 //    routine to allow introspection
 const params = new URLSearchParams(location.hash.slice(1));
 document.domain = params.get('domain') || document.domain; // Let this be iframe-embeddable
 */
-
-// Todo: Reenable the following in `files.json`?
-// {"name": "collins", "file": {"$ref": "data/other-works/Collins.json"}, "schemaFile": "Collins.jsonschema", "metadataFile": "Collins.metadata"},
-
-// Todo: Add a favicon file
-// import loadStylesheets from '../node_modules/load-stylesheets/dist/index-es.js';
-// loadStylesheets('favicon.ico'); // No need for `await`
 
 (async () => {
 const tb = new TextBrowser({
