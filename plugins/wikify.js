45c4cade747d0cd9a449d2536948720fdeba982d
Brett Zamir <brettz9@yahoo.com>
2018-04-19 18:44:59 +0800

    - Wikify plugin (including width/height and minimalist skin, and fitting cell more to content)

--- plugins/wikify.js
+++ plugins/wikify.js
@@ -3,7 +3,13 @@ export const escapeColumn = false;
 
 export const getCellData = function ({
     applicableFieldText, tr,
-    fieldLang, meta
+    fieldLang, meta, metaApplicableField
 }) {
-    return ``;
+    let style = metaApplicableField.width ? `width: ${metaApplicableField.width};` : '';
+    if (metaApplicableField.height) {
+        style += `height: ${metaApplicableField.height};`;
+    }
+    return `<iframe ${style ? ` style="${style}"` : ''}src="${metaApplicableField.baseURL}${applicableFieldText}?useskin=chameleon">${
+        applicableFieldText
+    }</iframe>`;
 };
