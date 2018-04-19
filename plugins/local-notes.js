45bccdf2db344bb5ff56bca87fcef3394759b018
Brett Zamir <brettz9@yahoo.com>
2018-04-19 21:22:45 +0800

    - Some local-notes plugin work

--- plugins/local-notes.js
+++ plugins/local-notes.js
@@ -1,9 +1,22 @@
 
 export const escapeColumn = false;
 
+let eventSet = false;
+
 export const getCellData = function ({
     applicableFieldText, tr,
     fieldLang, meta
 }) {
-    return ``;
+    if (!eventSet) {
+        // Also a `selectstart` event
+        window.addEventListener('selectionchange', () => {
+            window.getSelection();
+        });
+        eventSet = true;
+    }
+    // Ideally support read-only mouseover of specific sentencnes with coordinates?
+
+    // height: -webkit-fill-available; // Works but we want to allow multiple
+    return `
+    <textarea style="width: 300px !important; height: 200px;"></textarea>`;
 };
