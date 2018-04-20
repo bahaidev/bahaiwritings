fa3c0e6410a62bdf1de94661fec10894140bede1
Brett Zamir <brettz9@yahoo.com>
2018-04-20 14:14:21 +0800

    - Initial implementation of local-storage-based notes plugin

--- plugins/local-notes.js
+++ plugins/local-notes.js
@@ -1,26 +1,85 @@
+const locales = {
+    'en-US': {
+        'loading': 'Loading...',
+        'error_retrieval_notes': 'There was an error retrieving notes',
+        'error_saving_notes': 'There was an error saving notes'
+    }
+};
+
+const $$ = (sel) => {
+    return [...document.querySelectorAll(sel)];
+};
+
+function getCanonicalID (textarea) {
+    return textarea.parentNode.parentNode.dataset.canonicalId;
+}
+
+const localNotesDatabase = 'textbrowser-local-notes';
 
 export const escapeColumn = false;
 
-let eventSet = false;
+// We do this here instead of synchronously in anticipation of
+//    possible async localStorage: https://domenic.github.io/async-local-storage/
+export const done = async ({$p}) => {
+    const lang = $p.get('lang', true);
+    const l = (key) => {
+        const locale = locales[lang] || locales['en-US'];
+        return locale[key];
+    };
 
-export const getCellData = function ({
-    applicableFieldText, tr,
-    fieldLang, meta
-}) {
-    if (!eventSet) {
-        // Also a `selectstart` event
-        window.addEventListener('selectionchange', () => {
-            window.getSelection();
-        });
-        eventSet = true;
+    // We could shift to individual keys for each item (though would be
+    //         much harder to export)
+    function getNotes () {
+        try {
+            return JSON.parse(localStorage.getItem(localNotesDatabase) || '{}');
+        } catch (err) {
+            alert(l('error_retrieval_notes'));
+        }
     }
-    // Todo: Ideally support read-only mouseover of specific sentences with coordinates?
-    // Todo: Ability for highlight to even span multiple cells vertically
-    // Todo: Ability to add/remove multiple notes within a cell
-    // Todo: Ability to obtain or display notes from a specific website (or via
-    //          `postMessage` in case it too is local)
+    function saveNotes (obj) {
+        try {
+            localStorage.setItem(localNotesDatabase, JSON.stringify(obj));
+        } catch (err) {
+            alert(l('error_saving_notes'));
+        }
+    }
+
+    $p.get('work');
+    const obj = getNotes();
+    $$('textarea[data-local-notes]').forEach((textarea) => {
+        const id = getCanonicalID(textarea);
+        textarea.value = obj[id] || '';
+        textarea.disabled = false;
+    });
+
+    window.addEventListener('change', ({target}) => {
+        if (!target.matches('textarea[data-local-notes]')) {
+            return;
+        }
+        const id = getCanonicalID(target);
+        console.log('id', id);
+        const obj = getNotes();
+        obj[id] = target.value;
+        saveNotes(obj);
+    });
+};
+
+export const getCellData = ({
+    fieldLang, meta, $p
+}) => {
+    const lang = $p.get('lang', true);
+    const l = (key) => {
+        const locale = locales[lang] || locales['en-US'];
+        return locale[key];
+    };
+
+    // Todo: We need a stable `data-local-notes-id`
 
     // height: -webkit-fill-available; // Works but we want to allow multiple
     return `
-    <textarea style="width: 300px !important; height: 200px;"></textarea>`;
+    <textarea
+        data-local-notes=""
+        disabled="disabled"
+        style="width: 300px !important; height: 200px;"
+    >${l('loading')}</textarea>`;
 };
