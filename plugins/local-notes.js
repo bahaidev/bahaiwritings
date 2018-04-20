a7bd98f72368cd43c51cf65506a3c301efa8a53f
Brett Zamir <brettz9@yahoo.com>
2018-04-20 14:33:10 +0800

    - Reimplement from localStorage to IndexedDB, adding to-dos

--- plugins/local-notes.js
+++ plugins/local-notes.js
@@ -1,8 +1,10 @@
 const locales = {
     'en-US': {
         'loading': 'Loading...',
-        'error_retrieval_notes': 'There was an error retrieving notes',
-        'error_saving_notes': 'There was an error saving notes'
+        'please_reload': 'Please reload this page for the latest version.',
+        'error_opening': 'There was an error opening the database. Please report the issue.',
+        'blocked_opening': 'The database is blocked due to another request being present on the database. Please try again later.',
+        'error_retrieval_transaction': 'There was an error during the transaction to retrieve items. Please try again later.'
     }
 };
 
@@ -18,48 +20,107 @@ const localNotesDatabase = 'textbrowser-local-notes';
 
 export const escapeColumn = false;
 
-// We do this here instead of synchronously in anticipation of
-//    possible async localStorage: https://domenic.github.io/async-local-storage/
-export const done = async ({$p}) => {
+export const done = async ({$p}) => { // , canonicalBrowseFieldNames
+    // Todo: Fetch locales
+    // const results = await fetch('locales.json');
+    // const locales = await results.json();
+
+    // Todo: Should probably utilize same i18n mechanism as TextBrowser (passed from it?)
     const lang = $p.get('lang', true);
     const l = (key) => {
         const locale = locales[lang] || locales['en-US'];
         return locale[key];
     };
 
-    // We could shift to individual keys for each item (though would be
-    //         much harder to export)
-    function getNotes () {
-        try {
-            return JSON.parse(localStorage.getItem(localNotesDatabase) || '{}');
-        } catch (err) {
-            alert(l('error_retrieval_notes'));
-        }
-    }
-    function saveNotes (obj) {
-        try {
-            localStorage.setItem(localNotesDatabase, JSON.stringify(obj));
-        } catch (err) {
-            alert(l('error_saving_notes'));
-        }
-    }
+    const workStore = 'work-' + $p.get('work');
+    const workIndex = 'work-retrieval-' + $p.get('work');
 
-    const work = $p.get('work');
-    const obj = getNotes();
-    $$('textarea[data-local-notes]').forEach((textarea) => {
-        const id = getCanonicalID(textarea);
-        textarea.value = obj[`${work}-${id}`] || '';
-        textarea.disabled = false;
+    // Todo: Accept alternative namespace through TextBrowser
+    const openReq = indexedDB.open(localNotesDatabase);
+    openReq.addEventListener('upgradeneeded', ({target: {result: db}}) => {
+        db.addEventListener('versionchange', () => {
+            if (!document.hasFocus()) {
+                location.reload();
+            } else {
+                alert(l('please_reload'));
+            }
+        });
+        // const keyPath = canonicalBrowseFieldNames; // E.g., book-chapter-verse
+        const keyPath = 'id';
+        const store = db.createObjectStore(workStore, {
+            keyPath,
+            autoIncrement: false
+        });
+        store.createIndex(workIndex, keyPath, {
+            unique: true,
+            multiEntry: false
+        });
+    });
+    openReq.addEventListener('error', () => {
+        alert(l('error_opening'));
+    });
+    openReq.addEventListener('blocked', () => {
+        alert(l('blocked_opening'));
+    });
+    openReq.addEventListener('success', ({target: {result: db}}) => {
+        const tx = db.transaction(workStore, 'readonly');
+        // tx.addEventListener('complete', () => {});
+        tx.addEventListener('error', () => {
+            alert(l('error_retrieval_transaction'));
+        });
+        const store = tx.objectStore(workStore);
+        const idx = store.index(workIndex);
+        // Todo: Ideally we'd get starts/ends, but these can vary for browse field set
+        // const req = idx.getAll(IDBKeyRange.bound(starts, ends));
+        const req = idx.getAll();
+        req.addEventListener('success', ({target: {result}}) => {
+            $$('textarea[data-local-notes]').forEach((textarea, i) => {
+                // textarea.value = result[i] || '';
+                const id = getCanonicalID(textarea);
+                // const canonicalBrowseFieldVals = id.split('-');
+                const matchingObj = result.find((obj) => {
+                    return obj.id === id;
+                });
+                textarea.value = (matchingObj && matchingObj.value) || '';
+                // textarea.value = result[i] || '';
+                textarea.disabled = false;
+            });
+        });
     });
 
-    window.addEventListener('change', ({target}) => {
-        if (!target.matches('textarea[data-local-notes]')) {
+    // Todo: Depending on config, allow this to be optional (i.e., readonly)
+    window.addEventListener('change', ({target: textarea}) => {
+        if (!textarea.matches('textarea[data-local-notes]')) {
             return;
         }
-        const id = getCanonicalID(target);
-        const obj = getNotes();
-        obj[`${work}-${id}`] = target.value;
-        saveNotes(obj);
+        const id = getCanonicalID(textarea);
+        // const canonicalBrowseFieldVals = id.split('-');
+
+        const openReq = indexedDB.open(localNotesDatabase);
+        openReq.addEventListener('success', ({target: {result: db}}) => {
+            const tx = db.transaction(workStore, 'readwrite');
+            // tx.addEventListener('complete', () => {});
+            tx.addEventListener('error', () => {
+                alert(l('error_retrieval_transaction'));
+            });
+            const store = tx.objectStore(workStore);
+            const obj = {id, value: textarea.value};
+            /*
+            canonicalBrowseFieldVals.forEach((browseFieldVal, i) => {
+                // Todo: Ensure multiple entries per verse are supported
+                obj[canonicalBrowseFieldNames[i]] = browseFieldVal;
+            });
+            */
+            store.put(obj);
+        });
+    });
+
+    // Not yet supported in Chrome (Firefox only)
+    // Also a `selectstart` event
+    window.addEventListener('selectionchange', () => {
+        const sel = window.getSelection();
+        // Todo: Use this to set (last focused?) path adjoining textarea
+        console.log('sel', sel);
     });
 };
 
@@ -71,6 +132,15 @@ export const getCellData = ({
         const locale = locales[lang] || locales['en-US'];
         return locale[key];
     };
+
+    // Todo: Ability to add/remove multiple notes within a cell and display existing
+
+    // Todo: Ability to highlight to even span multiple cells vertically
+    // Todo: Indicate specific sentences with coordinates
+
+    // Todo: Ability to obtain or display notes from a specific website (or via
+    //          `postMessage` in case it too is local), optionally PUT back
+    // Todo: Ideally support read-only mouseover of specific sentences with coordinates?
 
     // height: -webkit-fill-available; // Works but we want to allow multiple
     return `
