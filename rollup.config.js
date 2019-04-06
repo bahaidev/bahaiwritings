75d33c6f841e355887494710c4bd3645f5b3cc0c
Brett Zamir <brettz9@yahoo.com>
2019-04-06 14:17:47 +0800

    - Note

--- rollup.config.js
+++ rollup.config.js
@@ -6,6 +6,11 @@ export default [
         // By building from distribution file, we can avoid
         //   some fragility, since the only based-in dep. now
         //   is popper.js
+        // This is the file pointed to by tippy.js' `module`
+        //   in `package.json`, so we could check for that
+        //   and import that instead, but if that ever ends
+        //   up needing more build steps than this dist
+        //   file, it could require more work than this.
         input: 'node_modules/tippy.js/esm/index.all.js',
         output: {
             format: 'esm',
