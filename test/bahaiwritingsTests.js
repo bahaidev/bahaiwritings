7a194a93bb122fba08284a49af0008138f674b01
Brett Zamir <brettz9@yahoo.com>
2022-11-20 16:40:29 -0700

    test: metadata

--- test/bahaiwritingsTests.js
+++ test/bahaiwritingsTests.js
@@ -11,7 +11,7 @@
 */
 function validate (schema, data, extraSchemas = [], additionalOptions = {}) {
   const ajv = new Ajv({
-    // allowMatchingProperties: true,
+    allowMatchingProperties: true, // `localeKey` and `.*`
     ...additionalOptions
   });
   let valid;
@@ -58,8 +58,6 @@ async function asyncSetup () {
     'table.jsonschema'
   ];
 
-  // Todo: We could also check that the `table` and `fields`-pointed
-  //   metadata exists and is valid
   const results = await Promise.all([
     ...specificFiles.map((f) => {
       return JsonRefs.resolveRefsAt(
@@ -84,15 +82,49 @@ async function asyncSetup () {
     ...tableFiles.map(
       (f) => getJSON(path.join(__dirname, appBase,
         'node_modules/textbrowser-data-schemas/schemas/', f))
+    ),
+    ...['metadata.jsonschema', 'locale.jsonschema'].map(
+      (f) => JsonRefs.resolveRefsAt(
+        path.join(
+          __dirname,
+          appBase,
+          'node_modules/textbrowser-data-schemas/schemas/',
+          f
+        ), {
+          refPreProcessor (obj, pth) {
+            if (
+              obj.$ref === '#' &&
+              // Extra sanity check to ensure we're only changing for the
+              //  known self-reference we need to adjust
+              JsonRefs.pathToPtr(pth) === '#/patternProperties/.*/anyOf/2'
+            ) {
+              // We hard-code the absolute path since the `pth` supplied is
+              //  relative to the local (locale.jsonschema) document only,
+              //  and we want to get rid of this self-reference with JsonRefs
+              //  otherwise insists on adding as a file name reference (which
+              //  we don't want for ajv)
+              obj.$ref = JsonRefs.pathToPtr([
+                'properties', 'localization-strings'
+              ]);
+            }
+            return obj;
+          }
+        }
+      )
     )
   ]);
 
   let cursor = 0;
   const [
-    dataFiles, schemaFiles, otherDataFiles, otherSchemaFiles, [table]
+    dataFiles, schemaFiles,
+    otherDataFiles, otherSchemaFiles,
+    [tableSchemaFile],
+    [metadataSchemaFile]
   ] = [
-    specificFiles, specificFiles, otherSpecificFiles,
-    otherSpecificFiles, tableFiles
+    specificFiles, specificFiles,
+    otherSpecificFiles, otherSpecificFiles,
+    tableFiles,
+    [null]
   ].map((files) => {
     // eslint-disable-next-line no-return-assign -- Convenient
     return results.slice(cursor, cursor += files.length);
@@ -102,29 +134,40 @@ async function asyncSetup () {
     [
       '../../../node_modules/' +
         'textbrowser-data-schemas/schemas/table.jsonschema',
-      table
+      tableSchemaFile
     ]
   ];
+
+  // console.log('resolved', JSON.stringify(metadataSchemaFile.resolved));
 
   return {
     dataFiles,
     schemaFiles,
     otherDataFiles,
     otherSchemaFiles,
-    table,
     testSchemaFiles (desc, dtaFiles, schmaFiles) {
       describe(desc, function () {
-        dtaFiles.forEach(({resolved: {data}}, i) => {
+        dtaFiles.forEach(({resolved: {data, metadata}}, i) => {
           const schema = schmaFiles[i];
           const schemaName = (
             desc === 'Main files' ? specificFiles : otherSpecificFiles
           )[i];
+
           it('validates schema ' + schemaName, function () {
             const vald = validate(schema, data, tableSchema);
             assert.strictEqual(vald, true);
           });
 
-          // This doesn't do what I had hoped: https://github.com/ajv-validator/ajv/issues/2170
+          it('validates metadata ' + schemaName, function () {
+            const vald = validate(
+              metadataSchemaFile.resolved, metadata
+            );
+            assert.strictEqual(vald, true);
+          });
+
+          // This doesn't remove all as hoped as don't have
+          //   `additionalProperties: false` set
+          // https://github.com/ajv-validator/ajv/issues/2170
           // it('has no extra properties', function () {
           //   const data2 = cloneJSON(data);
           //   const vald2 = validate(schema, data2, tableSchema, {
@@ -143,14 +186,17 @@ async function asyncSetup () {
 
 const {
   dataFiles, otherDataFiles,
-  schemaFiles,
-  testSchemaFiles,
-  otherSchemaFiles
+  schemaFiles, otherSchemaFiles,
+  testSchemaFiles
 } = await asyncSetup();
 
 describe('bahaiwritings Tests', function () {
   this.timeout(50000);
 
-  testSchemaFiles('Main files', dataFiles, schemaFiles);
-  testSchemaFiles('Other works', otherDataFiles, otherSchemaFiles);
+  testSchemaFiles(
+    'Main files', dataFiles, schemaFiles
+  );
+  testSchemaFiles(
+    'Other works', otherDataFiles, otherSchemaFiles
+  );
 });
